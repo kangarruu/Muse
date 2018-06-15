c1ed392371f4603a3bdaed85963d4925541b0d47
Ru Levin <rulevin@gmail.com>
2018-06-14 22:49:44 -0400

    Added ImageView "buttons" to PlayingNow activity

--- app/src/main/java/com/example/android/muse/PlayingActivity.java
+++ app/src/main/java/com/example/android/muse/PlayingActivity.java
@@ -8,13 +8,15 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 
-public class PlayingActivity extends AppCompatActivity {
-
+public class PlayingActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +26,7 @@ public class PlayingActivity extends AppCompatActivity {
         Toolbar songsToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(songsToolbar);
         //enable up button functionality
-        if(getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
@@ -44,15 +46,46 @@ public class PlayingActivity extends AppCompatActivity {
 
             ImageView cover = this.findViewById(R.id.playing_image_view);
             cover.setImageResource(current_song.getImageResourceId());
-
-
         }
 
-    }
+        //Find the play button ImageViews via findViewByID
+        ImageView playButton = this.findViewById(R.id.play_image_view);
+        ImageView skipButton = this.findViewById(R.id.skip_image_view);
+        ImageView rwdButton = this.findViewById(R.id.rwd_image_view);
+
+        //Set onClickListeners on the views to display the toasts below
+        playButton.setOnClickListener(this);
+        skipButton.setOnClickListener(this);
+        rwdButton.setOnClickListener(this);
+
+      }
+
     //Overriding the toolbar onSupportNavigateUp method learned from https://freakycoder.com/android-notes-24-how-to-add-back-button-at-toolbar-941e6577418e to enable up button
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    //I learned this nifty way of overriding Toasts from here: https://codinginflow.com/tutorials/android/onclicklistener-for-multiple-buttons
+    //and https://stackoverflow.com/questions/2755277/android-hide-all-shown-toast-messages
+    @Override
+    public void onClick(View v) {
+        Toast clickedToast = Toast.makeText(this, " ", Toast.LENGTH_SHORT);
+        switch(v.getId()){
+            case R.id.play_image_view:
+                clickedToast.setText("La La La La Laaaaa");
+                clickedToast.show();
+                break;
+            case R.id.skip_image_view:
+                clickedToast.setText("Oh hey, another song");
+                clickedToast.show();
+                break;
+            case R.id.rwd_image_view:
+                clickedToast.setText("Again! Again!");
+                clickedToast.show();
+                break;
+        }
+
     }
 }
